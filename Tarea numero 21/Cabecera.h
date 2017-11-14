3abe9b20bb1a3fb718882491534573a0d1fe3b38
RiacardoBrandom <31740942+RiacardoBrandom@users.noreply.github.com>
2017-11-14 13:21:14 -0600

    Add files via upload

---
+++ Tarea numero 21/Cabecera.h
@@ -0,0 +1,10 @@
+void appInfoData(char *titulo,char *fecha)
+{
+  printf("----------------------------------------------------------------\n");
+  printf("_\n");
+  printf("_       %s\n", titulo);
+  printf("_       Riardo Brandom Gutierrez Molina®\n");
+  printf("_       %s\n", fecha);
+  printf("_\n");
+  printf("----------------------------------------------------------------\n");
+}
